bdd04271cd47e7f0f1f073dd90d2ff820f3ab031
Benjamin Hipple <benjamin.hipple@gmail.com>
2016-05-30 20:54:38 -0400

    Shows gitstats for one repo!

--- src/GitStats.hs
+++ src/GitStats.hs
@@ -8,15 +8,17 @@ import Data.Aeson
 import Data.Aeson.TH
 import Network.Wai
 import Network.Wai.Handler.Warp
-import Servant.HTML.Blaze
-import Text.Blaze.Html5 hiding (main)
 import Servant
+import Servant.HTML.Blaze
+import Servant.Utils.StaticFiles
+import Text.Blaze.Html5 hiding (main)
 
 data Page = Page
 instance ToMarkup Page where
     toMarkup Page = h1 "hello world"
 
-type API = Get '[HTML] Page
+type GsApi = "hello" :> Get '[HTML] Page
+        :<|> Raw
 
 startApp :: IO ()
 startApp = run 8080 app
@@ -24,8 +26,9 @@ startApp = run 8080 app
 app :: Application
 app = serve api server
 
-api :: Proxy API
+api :: Proxy GsApi
 api = Proxy
 
-server :: Server API
+server :: Server GsApi
 server = return Page
+      :<|> serveDirectory "/var/www/repos/gitstats"
